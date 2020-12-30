e05472d4b58973dbb4d68789c6b4825ad5d56ace
KailHel <kaiiilhell@gmail.com>
2020-12-31 01:52:07 +0300

    Передача стандартного пути + сведения об ошибках

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 
+
+#include <string>
 #include "game/Game.h"
 
-int main() {
+
+
+int main(int argc ,char ** argv) {
     {
+        const string a(argv[0]);
+        ResourcesManager::setDefaultPath(a);
         Game* game = new Game();
-        game->setFPS(15);
+        game->setFPS(59);
         game->start();
         delete game;
     }
     exit(0);
 }
 //status 0 - Normal Exit
+//status 3 - Shader Error Compile
